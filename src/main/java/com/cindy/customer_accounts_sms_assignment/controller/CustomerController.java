b1ba6e6883062f7bade142aebc93b290a6f08393
Muema <KEN20952@kcbgroup.com>
2022-06-08 10:56:46 +0300

    Add save customer method in Customercontroller.

--- src/main/java/com/cindy/customer_accounts_sms_assignment/controller/CustomerController.java
+++ src/main/java/com/cindy/customer_accounts_sms_assignment/controller/CustomerController.java
@@ -1,4 +1,17 @@
 package com.cindy.customer_accounts_sms_assignment.controller;
 
+import com.cindy.customer_accounts_sms_assignment.model.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class CustomerController {
+//create customer rest pi then convert to xml
+    @RequestMapping(value = "/createCustomer", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer){
+        return null;
+    }
+
 }
