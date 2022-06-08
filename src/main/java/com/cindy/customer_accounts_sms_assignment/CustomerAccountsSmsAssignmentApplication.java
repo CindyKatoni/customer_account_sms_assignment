0c51b6d1aed81520f9d535ec74a80eddafbfd357
Muema <KEN20952@kcbgroup.com>
2022-06-08 10:24:42 +0300

    First commit.

---
+++ src/main/java/com/cindy/customer_accounts_sms_assignment/CustomerAccountsSmsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cindy.customer_accounts_sms_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountsSmsAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAccountsSmsAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cindy/customer_accounts_sms_assignment/CustomerAccountsSmsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cindy.customer_accounts_sms_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountsSmsAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
